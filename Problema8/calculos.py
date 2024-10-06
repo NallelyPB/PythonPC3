c39252bde68de497d1cf63592e8a11dc68e2992d
NallelyPB <nallelyparedes6@gmail.com>
2024-10-06 05:26:03 +0000

    Solución del problema 8

---
+++ Problema8/calculos.py
@@ -0,0 +1,9 @@
+import operaciones
+
+a = 10
+b = 0
+
+print("Suma:", operaciones.suma(a, b))
+print("Resta:", operaciones.resta(a, b))
+print("Producto:", operaciones.producto(a, b))
+print("División:", operaciones.division(a, b))
